6b8e2fb40388a2dfd583acde9fd84e4d6e1f3ef5
Ryan Miyahara <rmiyahara144@gmail.com>
2019-09-12 02:09:56 -0700

    Added databases

--- main_page/models.py
+++ main_page/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.FilePathField(path="/Portfolio")
+    technology = models.CharField(max_length=20)
+    description = models.TextField()
+
+class Job(models.Model):
+    company = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    description1 = models.TextField()
+    description2 = models.TextField()
+    description3 = models.TextField()
+    description4 = models.TextField()
+
+class Skill(models.Model):
+    language = models.CharField(max_length=100)
--- main_page/views.py
+++ main_page/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from main_page.models import Project, Job, Skill
 
 # Create your views here.
 def main_page(request):
-    return render(request, 'main_page.html', {})
+    skills = Skill.objects.all()
+    context = {
+        'skills': skills
+    }
+    return render(request, 'main_page.html', context)

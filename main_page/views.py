befd72e730e6d11955201ec61f915575cfc3a8f9
Ryan Miyahara <rmiyahara144@gmail.com>
2019-09-16 17:01:24 -0700

    Added work experience section

--- main_page/views.py
+++ main_page/views.py
@@ -3,8 +3,10 @@ from main_page.models import Project, Job, Skill
 
 # Create your views here.
 def main_page(request):
+    jobs = Job.objects.all()
     skills = Skill.objects.all()
     context = {
+        'jobs': jobs,
         'skills': skills
     }
     return render(request, 'main_page.html', context)
